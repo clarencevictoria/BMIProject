c8cb79625997026670b151e38c62c58c9885d8ca
clarencevictoria <clarence.victoria@novaliches.sti.edu>
2014-01-16 14:24:43 +0800

    Updated User Class and added ComputeBMI Class

--- BodyMassIndex/src/com/bmi/BMIProject/User.java
+++ BodyMassIndex/src/com/bmi/BMIProject/User.java
@@ -4,25 +4,47 @@ class User {
 	
 	private double height;
 	private double weight;
+	private String fname;
+	private String lname;
 	
 	public User(){
 		super();
 	}
 	
-	public double getHeight(double height){
+	public double getHeight(){
 		return height;
 	}
 	
-	public void setHeight(){
+	public void setHeight(double height){
 		this.height = height;
 	}
 	
-	public double getWeight(double weight){
+	public double getWeight(){
 		return weight;
 	}
 	
-	public void setWeight(){
+	public void setWeight(double weight){
 		this.weight = weight;
 	}
 	
+	public String getFirstName(){
+		return fname;
+	}
+	
+	public void setFirstName(String fname){
+		this.fname = fname;
+	}
+	
+	public String getLastName(){
+		return lname;
+	}
+	
+	public void setLastName(String lname){
+		this.lname = lname;
+	}
+	
+	public String getFullName(){
+		String name = lname + ", " + fname;
+		return name;
+	}
 }
