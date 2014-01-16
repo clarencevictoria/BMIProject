9fe3e66269ccea20f575bcde3c51b8c5f3042df2
clarencevictoria <clarence.victoria@novaliches.sti.edu>
2014-01-16 14:03:29 +0800

    Added Project in the Repository

---
+++ BodyMassIndex/src/com/bmi/BMIProject/User.java
@@ -0,0 +1,28 @@
+package com.bmi.BMIProject;
+
+class User {
+	
+	private double height;
+	private double weight;
+	
+	public User(){
+		super();
+	}
+	
+	public double getHeight(double height){
+		return height;
+	}
+	
+	public void setHeight(){
+		this.height = height;
+	}
+	
+	public double getWeight(double weight){
+		return weight;
+	}
+	
+	public void setWeight(){
+		this.weight = weight;
+	}
+	
+}
